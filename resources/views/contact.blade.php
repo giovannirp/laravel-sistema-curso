5a4970a5a9d1f0ea042dd20e6ea657bb91c4c164
giovanni <giovanni_ge@hotmai.com>
2024-07-25 21:41:21 -0300

    feat: add

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<h1>Página contato</h1>
+
+<a href="/">Voltar pra home</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+
+Route::get('/contact', function () {
+    return view('contact');
 });
+
