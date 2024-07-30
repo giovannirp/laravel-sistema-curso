42b35545fa8fa72e4be7b12ef8cf63aa2f2301a9
giovanni <giovanni_ge@hotmai.com>
2024-07-29 21:50:37 -0300

    feat: add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,25 @@
 @section('title', 'Cursos e eventos')
 
 @section('content')
-
-    <h1>{{ $nome }}</h1>
-
-    <p>O nome é {{ $nome }} e ele tem {{ $idade}} anos</p>
-
     <div id="search-container" class="col-md-12">
         <h1 class="text-danger">Busque um evento</h1>
 
         <form action="">
             <input type="text" id="search" class="form-control" placehoder="Procurar..." />
         </form>
+    </div>
+
+  
+    <div class="cards-container row mt-5">
+        @foreach($events as $event)
+        <div class="card col-md-3 ml-4">
+            <img src="/img/img-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-text"> {{ $event->description }}</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        @endforeach
     </div>
 @endsection
